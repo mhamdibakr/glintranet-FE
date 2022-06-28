41a9df837c050f5e3e28b394c22f1720e3534550
Ismail MEGGOURI <i.meggouri@giantlink.ma>
2022-06-28 17:22:40 +0100

    docs list

--- src/app/main/document/components/all-docs/all-docs.module.ts
+++ src/app/main/document/components/all-docs/all-docs.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AllDocsComponent } from './all-docs.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { CoreCommonModule } from '@core/common.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
+import { CardSnippetModule } from '@core/components/card-snippet/card-snippet.module';
 
 
 const routes: Routes = [
@@ -17,7 +23,11 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    NgxDatatableModule,
+    CoreCommonModule,
+    NgbModule,
+    CardSnippetModule
   ]
 })
 export class AllDocsModule { }
