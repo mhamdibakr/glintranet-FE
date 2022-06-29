b79a1147715446ee6d2b9a1e17ca9b0133fcbe35
Ismail MEGGOURI <i.meggouri@giantlink.ma>
2022-06-29 15:37:26 +0100

    users & user

--- src/app/main/user/components/adduser/adduser.component.ts
+++ src/app/main/user/components/adduser/adduser.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AllCandidat } from 'app/main/CvTech/models/all-candidat.model';
+import { AllCandidatService } from 'app/main/CvTech/services/all-candidat.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-adduser',
@@ -7,9 +12,145 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdduserComponent implements OnInit {
 
-  constructor() { }
+  contentHeader: { headerTitle: string; actionButton: boolean; breadcrumb: { type: string; links: ({ name: string; isLink: boolean; link: string; } | { name: string; isLink: boolean; link?: undefined; })[]; }; };
+
+  public candidat = {
+    firstName: undefined,
+    lastName: undefined,
+    email: undefined,
+    phone: undefined,
+    username: undefined,
+    birthDate: undefined,
+    password: undefined,
+    cin: undefined
+  }
+
+
+  public form: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    username: new FormControl(''),
+    birthDate: new FormControl(false),
+    password: new FormControl(),
+    cin: new FormControl()
+  });
+  submitted = false;
+  constructor(private formBuilder: FormBuilder,
+    private AllCandidatService: AllCandidatService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+
+    this.contentHeader = {
+      headerTitle: 'User',
+      actionButton: true,
+      breadcrumb: {
+        type: '',
+        links: [
+          {
+            name: 'Home',
+            isLink: true,
+            link: '/'
+          },
+          {
+            name: 'Users',
+            isLink: true,
+            link: '/user/Allusers'
+          },
+          {
+            name: 'Add User',
+            isLink: false
+          }
+        ]
+      }
+    };
+
+    this.form = this.formBuilder.group(
+      {
+        firstName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern("[a-zA-Z ]*")
+          ]
+        ],
+        lastName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern("[a-zA-Z ]*")
+          ]
+        ],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern("[a-zA-Z ]*")
+          ]
+        ],
+        birthDate: ['', Validators.required],
+        cin: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        phone: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(/(\+212|0)([ \-_/]*)(\d[ \-_/]*){9}/g),
+
+          ]
+        ],
+        password: ['',
+          [
+            Validators.required,
+            Validators.minLength(6)
+          ]
+        ]
+      }
+    );
+
+
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    this.candidat = this.form.value;
+
+    // this.addNewCandidat();
+
+  }
+  // private addNewCandidat() {
+  //   this.AllCandidatService.addCandidat(this.candidat).subscribe(
+  //     {
+  //       next: (response: any) => {
+  //         console.log(response);
+  //         Swal.fire({
+  //           icon: 'success',
+  //           title: 'Your work has been saved',
+  //           showConfirmButton: false,
+  //           timer: 1500
+  //         });
+  //         this.router.navigateByUrl("/cvtech/candidats/allcandidats");
+  //       }, error: (err) => {
+  //         console.error(err);
+  //       }
+  //     }
+  //   );
+  // }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
   }
 
 }
--- src/app/main/user/components/adduser/adduser.module.ts
+++ src/app/main/user/components/adduser/adduser.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AdduserComponent } from './adduser.component';
+import { CoreCommonModule } from '@core/common.module';
+import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 
 const routes: Routes = [
@@ -14,7 +21,15 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    CoreCommonModule,
+    ContentHeaderModule,
+    NgbModule,
+    NgSelectModule,
+    FormsModule,
+    NgxDatatableModule,
+    ReactiveFormsModule,
+    SweetAlert2Module.forRoot()
   ]
 })
 export class AdduserModule { }
